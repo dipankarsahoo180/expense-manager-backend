4573a02f59aab56acdc8330b0dce4dc049074b29
Dipankar Sahoo <dipankarsahoo180@gmail.com>
2024-03-21 14:50:49 +0530

    oauth flow

--- src/config.js
+++ src/config.js
@@ -3,9 +3,11 @@ module.exports={
     SF_CLIENT_ID:process.env.SF_CLIENT_ID,
     SF_CLIENT_SECRET:process.env.SF_CLIENT_SECRET,
     SF_LOGIN_URL:process.env.SF_LOGIN_URL,
+    SF_REDIRECT_URL:process.env.SF_REDIRECT_URL,
     SF_USER_NAME:process.env.SF_USER_NAME,
     SF_PASSWORD:process.env.SF_PASSWORD,
     SF_SECURITY_TOKEN:process.env.SF_SECURITY_TOKEN,
     PORT: process.env.PORT,
     SERVER_URL :process.env.SERVER_URL,
+    FRONTEND_URL :process.env.FRONTEND_URL,
 }
---
+++ src/services/salesforceService.js
@@ -0,0 +1,41 @@
+
+
+const {SF_CLIENT_ID,SF_CLIENT_SECRET,SF_LOGIN_URL,SF_REDIRECT_URL,FRONTEND_URL}  = require('../config')
+var jsforce = require('jsforce');
+
+// OAuth2 client information can be shared with multiple connections.
+var oauth2 = new jsforce.OAuth2({
+  // you can change loginUrl to connect to sandbox or prerelease env.
+  loginUrl : SF_LOGIN_URL,
+  clientId : SF_CLIENT_ID,
+  clientSecret : SF_CLIENT_SECRET,
+  redirectUri : SF_REDIRECT_URL
+});
+
+const login = (req,res) => {
+  res.redirect(oauth2.getAuthorizationUrl({ scope : 'full' }));
+};
+
+const callback = (req,res) => {
+    const {code} = req.query;
+    console.log('authorization code is',code)
+    if(!code){
+        console.error("Failed to get authorization code from server callback")
+        return res.status(500).send("Failed to get authorization code from server callback")
+    }
+    // console.log("code", code)
+    const conn = new jsforce.Connection({oauth2:oauth2})
+    conn.authorize(code, function(err){
+        if(err){
+            console.error(err);
+            return res.status(500).send(err)
+        }
+        console.log("Access token", conn.accessToken)
+        console.log("Connection", conn)
+        console.log("Instance url", conn.instanceUrl)
+        // lcStorage.setItem('accessToken', conn.accessToken || '')
+        // lcStorage.setItem('instanceUrl', conn.instanceUrl || '')
+        res.redirect(FRONTEND_URL)
+    })
+}
+module.exports = {login,callback}
