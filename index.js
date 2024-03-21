ed1bf43b19b20ebd0c80611e083fd2b2e13bbd38
Dipankar Sahoo <dipankarsahoo180@gmail.com>
2024-03-21 14:17:37 +0530

    Revert "salesforce connection"
    This reverts commit 457e2be47a99e58c703507cc873f122688f212ba.

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express();
-const {PORT,SERVER_URL}  = require('./src/config')
-// const {SF_CLIENT_ID,SF_CLIENT_SECRET,SF_LOGIN_URL,SF_USER_NAME,SF_PASSWORD,SF_SECURITY_TOKEN,PORT,SERVER_URL}  = require('./src/config')
-const authController = require('./src/controllers/authController')
-// var jsforce = require('jsforce');
-// var conn = new jsforce.Connection({
-//     loginUrl:SF_LOGIN_URL
-// });
+const {SF_CLIENT_ID,SF_CLIENT_SECRET,SF_LOGIN_URL,SF_USER_NAME,SF_PASSWORD,SF_SECURITY_TOKEN,PORT,SERVER_URL}  = require('./src/config')
+
+var jsforce = require('jsforce');
+var conn = new jsforce.Connection({
+    loginUrl:SF_LOGIN_URL
+});
 
 
 app.listen(PORT,()=>{
@@ -14,14 +13,12 @@ app.listen(PORT,()=>{
 })
 
 app.get('/',async(req,res)=>{
-    // await conn.login(SF_USER_NAME, SF_PASSWORD+SF_SECURITY_TOKEN, function(err, res) {
-    //     if (err) { return console.error(err); }
-    //     conn.query('SELECT Id, Name FROM Account', function(err, res) {
-    //       if (err) { return console.error(err); }
-    //       console.log(res);
-    //     });
-    //   });
+    await conn.login(SF_USER_NAME, SF_PASSWORD+SF_SECURITY_TOKEN, function(err, res) {
+        if (err) { return console.error(err); }
+        conn.query('SELECT Id, Name FROM Account', function(err, res) {
+          if (err) { return console.error(err); }
+          console.log(res);
+        });
+      });
     res.send('<h1>Congrats</h1>')
 })
-
-app.use('/oauth',authController)
