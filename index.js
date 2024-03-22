7d2dfaa45c68f5148d7dac1ce3940d6295e313de
Dipankar Sahoo <dipankarsahoo180@gmail.com>
2024-03-23 00:59:22 +0530

    allow localhost 3000

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser')
-const alloweOrigins = [FRONTEND_URL,'http://localhost:300']
+const alloweOrigins = [FRONTEND_URL,'http://localhost:3000']
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 app.use(cors({origin:alloweOrigins}))
